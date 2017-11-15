2e4cfd82e6b20883922ae8c5aba3bc3f4ae0d97b
Angel Gabriel <32152863+AngelHR@users.noreply.github.com>
2017-11-14 23:18:11 -0600

    Add files via upload

---
+++ ConsoleApp8/ConsoleApp8/Areas.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Areas
+    {
+        private double Pi = 3.1416;
+        private double Altura { get; set; }
+        private double Base { get; set; }
+        private double Radio { get; set; }
+
+        public void Rectangulo(double p, double b)
+        {
+            Altura = p;
+            Base = b;
+        }
+
+        public double AreaRec()
+        {
+            double r =Altura * Base;
+            return r;
+        }
+        public void Ciculo()
+        {
+            Console.WriteLine("Dame el radio del circulo");
+            Radio = double.Parse(Console.ReadLine());
+            Console.WriteLine("Su area {0}",AreaC());
+        }
+        public double AreaC()
+        {
+            double r = Pi * Pot(Radio, 2); ;
+            return r;
+        }
+        public void Cono()
+        {
+            Console.WriteLine("Dame el radio del cono");
+            Radio = double.Parse(Console.ReadLine());
+            Console.WriteLine("Dame la altura del cono");
+            Altura = double.Parse(Console.ReadLine());
+            Console.WriteLine("Su area es: {0}",AreaC()*Altura/2);
+        }
+
+        public double Pot(double r, int e)
+        {
+            double res;
+            if (e == 0)
+                return 1;
+            else
+                res = r * Pot(r, e - 1);
+            return res;
+        }
+
+            public void Menu(Areas h, int n)
+        {
+
+                switch (n)
+                {
+                    case 1:
+                        int r, t;
+                        Console.WriteLine("Dame la base del Rectangulo\n");
+                        r = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Dame la base del Rectangulo\n");
+                        t = int.Parse(Console.ReadLine());
+                        Rectangulo(r, t);
+                        Console.WriteLine($"Su Area es:{ AreaRec()}\n");
+                       
+                        break;
+                    case 2:
+                        Ciculo();
+                        break;
+                    case 3:
+                        Cono();
+                        break;
+                    case 4:
+                        Console.WriteLine("Saliendo....\n");
+                        break;
+                    default:
+                        Console.WriteLine("Selecciona la opcion funcional.");
+                        break;
+                   
+                }
+           
+        }
+
+}
+}
---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Areas Area1 = new Areas();
+            do
+            {
+                Console.WriteLine("Que desea Obtener\n[1]Area De un Rectangulo\n[2]Area de un Circulo\n[3]Area del cono\n[4]Salir\n");
+                n = int.Parse(Console.ReadLine());
+                Area1.Menu(Area1, n);
+                if (n == 4)
+                    break;
+            }while(n != 4);
+        }
+    }
+}
